88f542929f61179c8d118af18d1c8b918e1882e0
Larry L <larrylight2003@msn.com>
2019-01-24 21:29:42 -0500

    this all bull

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ app.get("/api/google", function(req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
-app.listen(80, function() {
-  console.log("CORS-enabled web server listening on port 80");
-});
-
 app.get("/api/google");
 
 app.get(
